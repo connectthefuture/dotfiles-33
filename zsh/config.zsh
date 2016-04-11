cd6dbb22c915774c1a1bb7a9bc0dfc928ef7e345
Fredrik Averpil <fredrikaverpil@gmail.com>
2016-04-11 18:29:35 +0200

    Bundled all history related options together

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,5 +1,20 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+# http://bolyai.cs.elte.hu/zsh-manual/zsh_16.html
+# http://zsh.sourceforge.net/Guide/zshguide02.html
 
-# adding path directory for custom scripts
-export PATH=$DOTFILES/bin:$PATH
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt APPEND_HISTORY # zsh sessions will append their history list to the history file, rather than overwrite it
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS # tidy up the line when it is entered into the history by removing any excess blanks that mean nothing to the shell
+setopt NO_HIST_BEEP # shell won't beep when scrolled to top in editor
+setopt HIST_VERIFY # after a substitution the line appears again with the changes, instead of being immediately printed and executed
+
+setopt NO_BG_NICE # don't run background commands at lower priority
+setopt NO_HUP # don't kill background commands when exiting shell
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
+setopt CORRECT # try to correct the spelling of commands
+setopt IGNORE_EOF # do not exit on EOF
+setopt COMPLETE_ALIASES #  aliases on the command line are not internally substituted before completion is attempted
