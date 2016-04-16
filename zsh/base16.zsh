d9ec3c5b8a213768ef64d2165f060793d34636a9
Fredrik Averpil <fredrik@averpil.local>
2016-04-16 13:39:11 +0200

    Nice colors thanks to base16-shell

---
+++ zsh/base16.zsh
@@ -0,0 +1,23 @@
+# Base16 Shell
+# https://github.com/chriskempson/base16-shell
+
+# if [ -z "$THEME" ]; then
+    export THEME="base16-embers"
+# fi
+if [ -z "$BACKGROUND" ]; then
+    export BACKGROUND="dark"
+fi
+
+BASE16_SHELL="$DOTFILES/modules/base16-shell/$THEME.$BACKGROUND.sh"
+# [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+source $BASE16_SHELL
+
+
+# set the background color to light on "light"
+function light() {
+    export BACKGROUND="light" && reload!
+}
+# set the background color to dark on "dark"
+function dark() {
+    export BACKGROUND="dark" && reload!
+}
