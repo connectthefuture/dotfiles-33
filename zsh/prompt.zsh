5007429336d784a99cf73eba9ae2c5e075056d63
Fredrik Averpil <fredrik@averpil.local>
2016-04-16 09:40:10 +0200

    Make dirs prior to attempting to create symlinks

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,18 +2,21 @@
 # https://github.com/sindresorhus/pure
 
 # If symlink does not exist, create it
-local pure=$DOTFILES/zsh/zfunctions/prompt/prompt_pure_setup
+DOTFILES=$HOME/.dotfiles
+pure=$DOTFILES/zsh/zfunctions/prompt/prompt_pure_setup
 if [ ! -e "$pure" ]
 then
-    mkdir -p $DOTFILES/zsh/zfunctions
+    echo "Creating prompt_pure_setup symlink"
+    mkdir -p $DOTFILES/zsh/zfunctions/prompt
     ln -s $DOTFILES/modules/pure/pure.zsh $DOTFILES/zsh/zfunctions/prompt/prompt_pure_setup
 fi
 
 # If symlink does not exist, create it
-local async=$DOTFILES/zsh/zfunctions/prompt/async
+async=$DOTFILES/zsh/zfunctions/prompt/async
 if [ ! -e "$async" ]
 then
-    mkdir -p $DOTFILES/zsh/zfunctions
+    echo "Creating async symlink"
+    mkdir -p $DOTFILES/zsh/zfunctions/prompt
     ln -s $DOTFILES/modules/pure/async.zsh $DOTFILES/zsh/zfunctions/prompt/async
 fi
 
