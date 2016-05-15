47059f59b29be3e4a4785109d6d7eaeb5368eb62
Fredrik Averpil <Fredrik Averpil>
2016-05-15 20:01:16 +0200

    Fixed expression error

--- git/install.sh
+++ git/install.sh
@@ -2,8 +2,11 @@ echo "Install script: git"
 
 if test ! $(which git)
 then
-    brew install git
-elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-then
-    sudo dnf install git
+    if test "$(uname)" = "Darwin"
+    then
+        brew install git
+    elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+    then
+        sudo dnf install git
+    fi
 fi
