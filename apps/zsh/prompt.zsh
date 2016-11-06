647835e5eb92105a4510e55461d211b3c9fb8625
Fredrik Averpil <fredrik.averpil@gmail.com>
2016-11-06 13:47:56 +0100

    Fix zsh paths

--- apps/zsh/prompt.zsh
+++ apps/zsh/prompt.zsh
@@ -3,25 +3,25 @@
 
 # If symlink does not exist, create it
 DOTFILES=$HOME/.dotfiles
-pure=$DOTFILES/zsh/zfunctions/prompt/prompt_pure_setup
+pure=$DOTFILES/apps/zsh/zfunctions/prompt/prompt_pure_setup
 if [ ! -e "$pure" ]
 then
     echo "Creating prompt_pure_setup symlink"
-    mkdir -p $DOTFILES/zsh/zfunctions/prompt
-    ln -s $DOTFILES/modules/pure/pure.zsh $DOTFILES/zsh/zfunctions/prompt/prompt_pure_setup
+    mkdir -p $DOTFILES/apps/zsh/zfunctions/prompt
+    ln -s $DOTFILES/modules/pure/pure.zsh $DOTFILES/apps/zsh/zfunctions/prompt/prompt_pure_setup
 fi
 
 # If symlink does not exist, create it
-async=$DOTFILES/zsh/zfunctions/prompt/async
+async=$DOTFILES/apps/zsh/zfunctions/prompt/async
 if [ ! -e "$async" ]
 then
     echo "Creating async symlink"
-    mkdir -p $DOTFILES/zsh/zfunctions/prompt
-    ln -s $DOTFILES/modules/pure/async.zsh $DOTFILES/zsh/zfunctions/prompt/async
+    mkdir -p $DOTFILES/apps/zsh/zfunctions/prompt
+    ln -s $DOTFILES/modules/pure/async.zsh $DOTFILES/apps/zsh/zfunctions/prompt/async
 fi
 
 # Add zfunctions to fpath
-fpath=( "$DOTFILES/zsh/zfunctions/prompt" $fpath )
+fpath=( "$DOTFILES/apps/zsh/zfunctions/prompt" $fpath )
 
 # Load
 autoload -U promptinit && promptinit
