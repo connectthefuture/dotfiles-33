dc281a85ee5a44869acb8c9a46729ea773f1a52a
Fredrik Averpil <fredrik.averpil@gmail.com>
2016-11-07 11:11:15 +0100

    Add tree

---
+++ apps/tree/dotinstall.sh
@@ -0,0 +1,12 @@
+echo "Install script: tree"
+
+if test ! $(which tree)
+then
+    if test "$(uname)" = "Darwin"
+    then
+        brew install tree
+    elif test $(which apt-get)
+    then
+        sudo apt-get install tree
+    fi
+fi
