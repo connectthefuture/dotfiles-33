6200c28da3325998abb04f16e129beb37beb88b8
Fredrik Averpil <fredrik.averpil@gmail.com>
2016-11-07 09:08:40 +0100

    Add github ssh keys aliases

--- apps/bash/aliases.sh
+++ apps/bash/aliases.sh
@@ -1,2 +1,7 @@
 # Color ouput on OS X
 alias ll="ls -alhG"
+
+# SSH Keygen
+alias ssh_github="eval \"$(ssh-agent -s)\" && chmod 400 ~/.ssh/*id_rsa && ssh-add ~/.ssh/github_id_rsa"
+alias ssh_github-work="eval \"$(ssh-agent -s)\" && chmod 400 ~/.ssh/*id_rsa && ssh-add ~/.ssh/github-work_id_rsa"
+alias ssh_gitlab="eval \"$(ssh-agent -s)\" && chmod 400 ~/.ssh/*id_rsa && ssh-add ~/.ssh/gitlab_id_rsa"
